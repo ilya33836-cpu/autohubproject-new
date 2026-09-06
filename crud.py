b7ca8ec6735f73f52f206c7de66b54339c695dc2
topellclaude-web <topellclaude@gmail.com>
2026-09-06 22:20:06 +0300

    Make username lookup case-insensitive

--- crud.py
+++ crud.py
@@ -15,7 +15,7 @@ def get_user(db: Session, user_id: int):
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(models.User).filter(func.lower(models.User.username) == func.lower(username)).first()
 
 
 def get_user_by_email(db: Session, email: str):
